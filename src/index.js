980a281938119932ae03af5014966c89a5ec77ea
Gobind <gobindjeet78@gmail.com>
2022-12-28 12:57:25 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
-
+import { ChakraProvider,theme } from '@chakra-ui/react';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
- 
+  <ChakraProvider theme={theme}>
     <App />
- 
+  </ChakraProvider>
 );
-
+export const server = `https://api.coingecko.com/api/v3`;
